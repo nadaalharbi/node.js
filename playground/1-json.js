2be77c96bc1997f56824e54642efbf9b63fac450
Nada Alharbi <50887127+nadaalharbi@users.noreply.github.com>
2020-07-03 00:02:27 +0300

    Add files via upload

---
+++ playground/1-json.js
@@ -0,0 +1,34 @@
+const fs = require("fs")
+const book = {
+    title: "The power of now",
+    author: "Eckhart"
+}
+
+// stringify() takes a string and convert it to JSON Obj
+const bookJSON = JSON.stringify(book)
+console.log("This is the json object: "+ bookJSON)
+
+// parse() takes a JSON Obj and convert it to string 
+const bookParse = JSON.parse(bookJSON)
+console.log("This is the string of json object: "+ bookParse.title)
+
+//to write a file that contain the json object
+fs.writeFileSync("1-json.json",bookJSON)
+//to read and print a file that contain the json object:
+
+//1- Read it and strore it
+//2- Convert it from Binary to String
+//3- Parse it using JSON (from json Object to string)
+const dataBuffer = fs.readFileSync("1-json.json")
+const dataJSON = dataBuffer.toString()
+const data = JSON.parse(dataJSON)
+console.log(data.title+" , "+data.author)
+//4- to Change values we need to overwrite our origin data
+
+data.title = "Fangirl"
+data.author = "Rainbow"
+
+const newBook = JSON.stringify(data)
+fs.writeFileSync("1-json.json",newBook)
+console.log(data.title+" , "+data.author)
+
---
+++ playground/5-es6-objects.js
@@ -0,0 +1,38 @@
+const name = "Nada"
+const age = 23
+const myMajor = "CS"
+
+const user = {
+    name,//if same name letter it's OK to write it once
+    age,
+    major: myMajor// diff. name so u need to write it this way(Key:Value)
+}
+
+console.log("User inofrmation", user)
+
+
+//Object Destructuring :- 
+
+const product = {
+    label: "black notebook",
+    price: 5,
+    stock: 200,
+    salePrice: undefined
+}
+/*
+const label = product.label
+const price = product.price
+ 
+OR:
+*/
+const {label: productLabel,price} = product
+
+console.log(productLabel)
+console.log(price)
+
+//Another usage of Destruction when passing object to func
+const transaction = (order, {label,price})=>{
+console.log("Order number is: "+order+ "\nLabel: "+label+"\nPrice: "+price+"$")
+}
+
+transaction("order2", product)
