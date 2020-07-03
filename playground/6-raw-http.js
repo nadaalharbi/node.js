fe560989375ea07adc581a5765359f995aee5aca
NadaAlharbi <alharbinada22@gmail.com>
2020-07-03 05:20:25 +0300

    update raw-http file

--- playground/6-raw-http.js
+++ playground/6-raw-http.js
@@ -1,20 +1,43 @@
 const http = require('http')
-const url = 'http://api.weatherstack.com/current?access_key=d34cdfa26acff26df282cacf321ddd1d&query=ry&units=m';
+const port = process.env.PORT || 3000;
 
-const request = http.request(url, (response) => {
-    let data = ''
-    response.on('data', (chunk) => {
-        data = data + chunk.toString();
-        console.log(chunk)
-    });
-    response.on('end', () => {
-        const body = JSON.parse(data);
-        console.log(body);
+// Creates an Http GET request to static url, to GET the text then print it in terminal
+const options = {
+    hostname: 'webcode.me',
+    port: 80,
+    path: '/',
+    method: 'GET'
+};
 
-    });
-    request.on('error', (error) => {
-        console.log(`An error occured. ${error}`);
+const request = http.get(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+    res.on('data', (data) => {
+        process.stdout.write(data);
     });
 });
 
+request.on('error', (error) => {
+    console.error(error);
+});
+
 request.end();
+
+//-------------------------------------
+
+// Create server to print the current date as JSON response
+const server = http.createServer((req, res) => {
+    if (req.url == '/date') {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.write(JSON.stringify({ now: new Date() }));
+        res.end();
+    } else {
+        // if wrong url 
+        res.end('Invalid request');
+    }
+}).listen(port)
+//server.listen(port);
+
+console.log(`Server running on http://localhost:${port}/date`);
+
+
+
--- weather-app/src/app.js
+++ weather-app/src/app.js
@@ -5,7 +5,7 @@ const forecast = require('./utils/forcast');
 const app = express();
 
 const publicDirPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 
 
