c85b33a1e8663134178448102a7a9b351fce2e18
NadaAlharbi <alharbinada22@gmail.com>
2020-07-03 06:10:21 +0300

    create os module file

---
+++ playground/8-os-module.js
@@ -0,0 +1,33 @@
+const os = require('os'); //core module
+  
+// Returns the cpu architecture 
+console.log(`CPU architecture: ${os.arch()}`); 
+  
+// Returns the amount of free system memory in bytes 
+console.log(`Free memory: ${os.freemem()}`); 
+  
+// Returns total amount of system memory in bytes
+console.log(`Total memory: ${os.totalmem()}`); 
+  
+// Returns the list of network interfaces 
+console.log(`List of network Interfaces: ${os.networkInterfaces()}`); 
+  
+
+
+// Returns the operating systems default directory for temp files. 
+console.log(`OS default directory for temp files : ${os.tmpdir()}`); 
+
+// Return the endianness of sytem 
+console.log(`Endianness of system: ${os.endianness()}`); 
+  
+// Returns hostname of system 
+console.log(`Hostname: ${os.hostname()}`); 
+  
+// Return operating system name 
+console.log(`Operating system name: ${os.type()}`); 
+  
+// Returns the platform of os 
+console.log('operating system platform: ' + os.platform()); 
+  
+// Returns the operating systems release. 
+console.log(`OS release: ${os.release()}`); 
