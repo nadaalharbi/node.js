372daf7a3d7de89249377af18f9b4ab413f6a611
sy <sy@Nadas-MacBook-Pro.local>
2020-07-09 06:06:52 +0300

    create culster file

---
+++ playground/culster.js
@@ -0,0 +1,45 @@
+const cluster = require('cluster');
+const http = require('http');
+const app = require('express')();
+const os = require('os');
+const port = process.env.PORT || 3000
+
+
+
+
+//using Cluster module with simple http server
+if (cluster.isMaster) {
+    const numOfCPUs = os.cpus().length;
+    console.log(`Master cluster has ${numOfCPUs} number of CPUs.`);
+
+    //forking one processes for each available core on the system.
+    for (let i = 0; i < numOfCPUs; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('online', (worker) => {
+        console.log(`Worker ${worker.process.pid} is online`);
+    });
+
+    //In case any process dies either accidentally or killed by the user
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died with code: ${code}, and signal: ${signal}`);
+        // a new process is forked
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+
+    app.server = http.createServer((req, res) => {
+        const processIDMsg = `process ${process.pid} says hello!`;
+        console.log(processIDMsg);
+        res.end(processIDMsg);
+
+        //applied to all routes 
+        app.all('/*', (req, res) => {
+            res.send(processIDMsg).end();
+        });
+    });
+    app.server.listen(port);
+
+}
